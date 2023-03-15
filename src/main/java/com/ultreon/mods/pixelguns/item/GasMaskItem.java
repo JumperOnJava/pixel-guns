f1d38849acdcebea2002014c84a8e045dd54e570
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-03-14 20:06:24 -0400

    Added functionality to gas mask

--- src/main/java/com/ultreon/mods/pixelguns/item/GasMaskItem.java
+++ src/main/java/com/ultreon/mods/pixelguns/item/GasMaskItem.java
@@ -2,15 +2,18 @@ package com.ultreon.mods.pixelguns.item;
 
 import com.ultreon.mods.pixelguns.armor.HazardArmor;
 import com.ultreon.mods.pixelguns.client.GeoRendererGenerator;
-import com.ultreon.mods.pixelguns.entity.GasEntity;
-
+import com.ultreon.mods.pixelguns.util.LivingEntityAccessor;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.item.BuiltinModelItemRenderer;
+import net.minecraft.entity.AreaEffectCloudEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.LlamaSpitEntity;
+import net.minecraft.entity.projectile.thrown.PotionEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.animatable.client.RenderProvider;
@@ -20,6 +23,8 @@ import software.bernie.geckolib.renderer.GeoArmorRenderer;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -28,15 +33,28 @@ public class GasMaskItem extends HazardArmor implements GeoItem {
     public GasMaskItem() {
         super(EquipmentSlot.HEAD);
     }
-    
+
     @Override
-    public void inventoryTick(ItemStack itemStack, World world, Entity entity, int i, boolean bl) {
+    public int getItemBarColor(ItemStack stack) {
+        return MathHelper.packRgb(114, 164, 161);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         if (entity instanceof ServerPlayerEntity player) {
-            if (player.getInventory().armor.get(EquipmentSlot.HEAD.getEntitySlotId()) == itemStack && !world.getEntitiesByClass(GasEntity.class, entity.getBoundingBox(), a -> true).isEmpty()) {
-                if (itemStack.getDamage() >= itemStack.getMaxDamage()) {
-                    itemStack.decrement(1);
-                } else {
-                    itemStack.setDamage(itemStack.getDamage() + 1);
+            List<Entity> entities = new ArrayList<>();
+            entities.addAll(world.getEntitiesByClass(AreaEffectCloudEntity.class, entity.getBoundingBox(), cloud -> true));
+            entities.addAll(world.getEntitiesByClass(PotionEntity.class, entity.getBoundingBox(), potion -> true));
+            entities.addAll(world.getEntitiesByClass(LlamaSpitEntity.class, entity.getBoundingBox(), spit -> true));
+
+            if (player.getInventory().armor.get(EquipmentSlot.HEAD.getEntitySlotId()) == stack && !player.isCreative() && !entities.isEmpty()) {
+                LivingEntityAccessor accessor = ((LivingEntityAccessor) player);
+                if (stack.getDamage() < stack.getMaxDamage()) {
+                    stack.damage(1, player.getRandom(), player);
+                    accessor.isAffectedBySplashPotions(false);
+                }
+                else {
+                    accessor.isAffectedBySplashPotions(true);
                 }
             }
         }
---
+++ src/main/java/com/ultreon/mods/pixelguns/util/LivingEntityAccessor.java
@@ -0,0 +1,8 @@
+package com.ultreon.mods.pixelguns.util;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+public interface LivingEntityAccessor {
+
+    void isAffectedBySplashPotions(boolean isAffectedBySplashPotions);
+}
