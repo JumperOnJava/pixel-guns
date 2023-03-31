506df5d57e50998df5e62462a9d14912a2042ba1
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-03-30 20:17:52 -0400

    Removed EventHandlerRegistry

--- src/main/java/com/ultreon/mods/pixelguns/PixelGuns.java
+++ src/main/java/com/ultreon/mods/pixelguns/PixelGuns.java
@@ -1,5 +1,7 @@
 package com.ultreon.mods.pixelguns;
 
+import com.ultreon.mods.pixelguns.client.handler.RecoilHandler;
+import com.ultreon.mods.pixelguns.event.GunEvents;
 import com.ultreon.mods.pixelguns.registry.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
@@ -20,10 +22,11 @@ public class PixelGuns implements ModInitializer {
         RecipeRegistry.init();
         ScreenHandlerRegistry.init();
 
-        EventHandlerRegistry.registerEventHandlers();
         PacketRegistry.SERVER.registerPackets();
         ConfigRegistry.registerConfig();
         ItemGroupRegistry.registerItemGroups();
+
+        GunEvents.GUN_SHOT_POST.registerListener(RecoilHandler::onGunFire);
 
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
             client.execute(PixelGunsClient.TRACKED_GUN_COOLDOWNS::clear);
